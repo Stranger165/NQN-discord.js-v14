348dbcc69894df5eee103cd39f493d599633a9d2
cortex <75237415+Stranger165@users.noreply.github.com>
2021-08-15 12:06:36 +0530

    🍻fix

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 const { Client, Discord } = require("discord.js");
 const client = new Client({
-  intents: [
-      "GUILDS",
-      "GUILD_MEMBERS",
-      "GUILD_BANS",
-      "GUILD_INTEGRATIONS",
-      "GUILD_WEBHOOKS",
-      "GUILD_INVITES",
-      "GUILD_VOICE_STATES",
-      "GUILD_PRESENCES",
-      "GUILD_MESSAGES",
-      "GUILD_MESSAGE_REACTIONS",
-      "GUILD_MESSAGE_TYPING",
-      "DIRECT_MESSAGES",
-      "DIRECT_MESSAGE_REACTIONS",
-      "DIRECT_MESSAGE_TYPING",
-  ],
+    intents: 32767,
 });
 const { token } = require("./config.json")
 
